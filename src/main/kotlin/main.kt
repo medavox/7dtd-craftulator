2a3b7cd583e48f51a946c3bd31eb83d571e21990
Adam Howard <achdisposable1+github@gmail.com>
2020-09-19 10:09:48 +0100

    first working version of algo
    the output is still only on the console

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -19,41 +19,54 @@ private val toCraft = CountingMap<String>()
  * @param name the item's name, as it appears (if it does) in `recipes.xml`
  */
 fun Document.visitItem(name:String, quantity:Int) {
-    println("recipe[name~=$name]")
-    val itemsToCraft:NodeList = this.querySelectorAll("recipe[name~=$name]")
-    if(itemsToCraft.length == 0) {//no recipes matched
-        //either the search was bollocks, or the item was uncraftable
+    val recipeCandidates:NodeList = this.querySelectorAll("recipe[name~=$name]")
+    if(recipeCandidates.length == 0) {//no recipes matched
+        //either the search is bollocks, or the item IS uncraftable
         //add it to the uncraftables
 
         uncraftables[name] += quantity
-    } else {
-        toCraft[name] += quantity
-        //for now as a placeholder, select the first item in the list
-        val item: Node = itemsToCraft.item(0)!!
-
-        println("item: $item")
-        println("ingredients in its recipe:")
-
-        //now for the meaty bit:
-        //for each ingredient required by this recipe,
-        //find that ingredient's own recipe, and its recipe's ingredients, and so on.
-        //recursively find all recipes and materials needed to make the chosen recipe.
-        //if any ingredient can't be found in the supplied recipes.xml,
-        //then we assume it's uncraftable/obtain-only.
-        //along with the obvious (like engines and electrical parts),
-        //this also includes materials you mine (such as ores).
-        //so we'll end up with a total list of materials to acquire,
-        //and a list of things to craft from them
-        //(preferably in order).
-        //also consider `item.children`
-        for (i in 0 until item.childNodes.length) {
-            val ingredient = item.childNodes[i]
-            println(ingredient)
-
-        }
+        return
     }
-    println("uncraftables: $uncraftables")
-    println("to craft: $toCraft")
+
+    val filtered = recipeCandidates.asList().filter {
+        it is Element && it.tagName == "recipe" }.map { it as Element }
+    //println("possible candidates for crafting this item:"+filtered.size)
+    //for now as a placeholder, select the first valid recipe in the list
+    val item: Node = filtered[0]
+
+    println("ingredients needed to craft $quantity ${(item as Element).attributes["name"]?.value}:")
+
+
+    //now for the meaty bit:
+    //for each ingredient required by this recipe,
+    //find that ingredient's own recipe, and its recipe's ingredients, and so on.
+    //recursively find all recipes and materials needed to make the chosen recipe.
+    //if any ingredient can't be found in the supplied recipes.xml,
+    //then we assume it's uncraftable/obtain-only.
+    //along with the obvious (like engines and electrical parts),
+    //this also includes materials you mine (such as ores).
+    //so we'll end up with a total list of materials to acquire,
+    //and a list of things to craft from them
+    //(preferably in order).
+    //also consider `item.children`
+    val ingredients:List<Element> = item.childNodes.asList().
+        filter { it is Element && it.tagName == "ingredient" }.map { it as Element }
+    if(ingredients.isEmpty()) {//recipes with - ingredients are also uncraftable,
+        //rather than craftable without any ingredients
+        uncraftables[name] += quantity
+        return
+    }
+    toCraft[name] += quantity
+    for (ingredient in ingredients) {
+        println("\t${ingredient.attributes["count"]?.value?.toInt()?.times(quantity)} " +
+                "${ingredient.attributes["name"]?.value}")
+        //get ingredient name and count needed
+    }
+    for (ingredient in ingredients) {
+        this.visitItem(ingredient.attributes["name"]!!.value,
+                ingredient.attributes["count"]!!.value.toInt() * quantity)
+    }
+
 }
 
 fun main() {
@@ -88,14 +101,21 @@ fun gotRecipesXml(recipesXmlContents:String) {
 
     //xmlDoc.visitItem("vehicle", 1)
     xmlDocument.visitItem("vehicleGyrocopterPlaceable", 1)
+    println("uncraftables: $uncraftables")
+    println("to craft: $toCraft")
 }
 
-private fun Element.asString():String {
-
-    return "Node \"$nodeName\"; ${this.attributes.asList().fold(""){ acc, elem ->
-        "$acc / ${elem.asString()}"
-        
-    }}"
+private fun Node.asString():String {
+    return when(this){
+        is Element -> {
+            "Element \"$nodeName\"; "+this.attributes.asList().
+            fold("") { acc, elem ->
+                "$acc / ${elem.asString()}"
+            }
+        }
+        is Text -> "Text node \""+this.wholeText+"\""
+        else -> "Node \"$nodeName\"=\"$nodeValue\""
+    }
 }
 
 private fun Attr.asString() :String {
